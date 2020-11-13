d6f391cb73077530766c3d208fe67c2bd4c622a7
jhonatan <jhonatanferreira13@hotmail.com>
2020-11-13 12:24:27 -0300

    - Old home screen backupr

---
+++ App2.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { StyleSheet, View, Image } from 'react-native'
+import { Text } from 'react-native-elements'
+/* import { Colors } from 'react-native/Libraries/NewAppScreen' */
+import { clientConfig } from './scrpit-test/buildConfig'
+import logo from './scrpit-test/images/logo.jpg'
+import img1 from './scrpit-test/images/img1.jpg'
+import img2 from './scrpit-test/images/img2.jpg'
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>
+          teste
+        </Text>
+        <Text h1 style={{ color: clientConfig.clientThemeColor }}>
+          {clientConfig.clientName}
+        </Text>
+        <Text>
+          {clientConfig.clientSignInText}
+        </Text>
+        <Text>
+          {clientConfig.clientHomeText}
+        </Text>
+        <Text>
+          {clientConfig.clientId}
+        </Text>
+
+        <Image
+          source={logo}
+          style={{ width: 150, height: 150 }}
+        />
+        <Image
+          source={img1}
+          style={{ width: 150, height: 150 }}
+        />
+        <Image
+          source={img2}
+          style={{ width: 150, height: 150 }}
+        />
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15
+  },
+  btn: {
+    width: 200,
+    marginTop: 20
+  },
+  button: {
+    borderRadius: 9,
+    height: 50
+  }
+})
+
+export default App
