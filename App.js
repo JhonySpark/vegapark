95fae224e0511f11543712ba80507925aabbfc86
jhonatan <jhonatanferreira13@hotmail.com>
2020-11-16 14:28:06 -0300

    ++ App

--- App.js
+++ App.js
@@ -1,87 +1,64 @@
-import React, { Component } from 'react'
-import {
-  Alert,
-  TextInput,
-  View,
-  StyleSheet,
-  Image,
-  ImageBackground,
-  Text
-} from 'react-native'
-import { Button } from 'react-native-elements'
-import { clientConfig } from './scrpit-test/buildConfig'
-import logo from './scrpit-test/images/logo.png'
-import bg from './scrpit-test/images/img2.jpg'
-export default class App extends Component {
-  constructor (props) {
-    super(props)
+import React from 'react'
+import { StyleSheet, View, Image } from 'react-native'
+import { Text } from 'react-native-elements'
+/* import { Colors } from 'react-native/Libraries/NewAppScreen' */
+import { clientConfig } from './script-test/buildConfig'
+import logo from './script-test/images/logo.png'
+import img1 from './script-test/images/img1.jpg'
+import img2 from './script-test/images/img2.jpg'
 
-    this.state = {
-      username: '',
-      password: ''
-    }
-  }
+const App = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>
+          teste
+        </Text>
+        <Text h1 style={{ color: clientConfig.clientThemeColor }}>
+          {clientConfig.clientName}
+        </Text>
+        <Text>
+          {clientConfig.clientSignInText}
+        </Text>
+        <Text>
+          {clientConfig.clientHomeText}
+        </Text>
+        <Text>
+          {clientConfig.clientId}
+        </Text>
 
-  onLogin () {
-    const { username, password } = this.state
-
-    Alert.alert('Credentials', `${username} + ${password}`)
-  }
-
-  render () {
-    return (
-      <ImageBackground
-        source={bg}
-        imageStyle={{ opacity: 0.2 }}
-        style={{
-          flex: 1,
-          resizeMode: 'cover',
-          justifyContent: 'center'
-        }}
-      >
-        <View style={styles.container}>
-          <Image
-            source={logo}
-            style={{ width: 150, height: 150, marginBottom: 40 }}
-          />
-          <Text style={{ marginBottom: 20, fontSize: 30, fontWeight: 'bold' }}>{clientConfig.clientName}</Text>
-          <TextInput
-            placeholder='Usuário'
-            style={styles.input}
-          />
-          <TextInput
-            placeholder='Senha'
-            secureTextEntry
-            style={styles.input}
-          />
-
-          <Button
-            title='Entrar'
-            style={styles.input}
-            containerStyle={{ width: 280 }}
-            buttonStyle={{
-              backgroundColor: clientConfig.clientThemeColor, height: 46
-            }}
-            onPress={this.onLogin.bind(this)}
-          />
-        </View>
-      </ImageBackground>
-    )
-  }
+        <Image
+          source={logo}
+          style={{ width: 150, height: 150 }}
+        />
+        <Image
+          source={img1}
+          style={{ width: 150, height: 150 }}
+        />
+        <Image
+          source={img2}
+          style={{ width: 150, height: 150 }}
+        />
+      </View>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
+    borderRadius: 15
   },
-  input: {
-    width: 280,
-    height: 45,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: clientConfig.clientThemeColor,
-    marginBottom: 10
+  btn: {
+    width: 200,
+    marginTop: 20
+  },
+  button: {
+    borderRadius: 9,
+    height: 50
   }
 })
+
+export default App
